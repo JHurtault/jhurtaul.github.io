5ed69a73279baa4bb1057c447b07c67719df938e
Jelani Hurtault <jelani.hurtault85@gmail.com>
2024-12-11 02:33:04 -0800

    Created a modal popup for user experience when submitting a form

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ document.getElementById("contact").addEventListener("submit", function (event) {
   const email = document.getElementById("email").value;
   const message = document.getElementById("message").value;
 
-  // Display message to user
-  alert(`Thank you, ${name}! I will contact you by ${email} shortly`);
+  // Display pop up box to user
+  const modal = document.getElementById("thank-you-modal");
+  const modalMessage = document.getElementById("modal-message");
+
+  // Message to user
+  modalMessage.textContent = `Thank you, ${name}! I will contact you by email ${email} shortly.`;
+
+  // Show the modal
+  modal.style.display = "block";
 
   // log user information to console
 
   console.log("Name:", name);
   console.log("Email:", email);
   console.log("Message:", message);
+
   // clear information from form once submitted
   document.getElementById("contact").reset();
+});
+
+// Close the popup box when the user clicks the close button
+document.getElementById("close-modal").addEventListener("click", function () {
+  const modal = document.getElementById("thank-you-modal");
+  modal.style.display = "none";
 });
